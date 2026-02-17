15a4d77e5687ea7585dfa9b6acc903f81cbe6078
Hamed <hamedpyn@gmail.com>
2026-02-17 04:29:20 +0330

    Add metadata

--- app/page.js
+++ app/page.js
@@ -5,6 +5,32 @@ import PrescriptionGlasses from "@/components/templates/Home/PrescriptionGlasses
 import HelpSection from "@/components/templates/Home/HelpSection";
 import Footer from "@/components/modules/Footer/Footer";
 
+export const metadata = {
+  title: "عینک رهام | فروشگاه تخصصی عینک",
+  description:
+    "خرید انواع عینک آفتابی و طبی با بهترین کیفیت و طراحی مدرن. دید بهتر و استایلی جذاب را با عینک رهام تجربه کنید.",
+  keywords: ["عینک", "عینک آفتابی", "عینک طبی", "فروشگاه عینک", "اپتیک"],
+  icons: {
+    icon: "./images/photo_2026-02-14_01-04-33.jpg",
+  },
+  openGraph: {
+    title: "عینک رهام | فروشگاه تخصصی عینک",
+    description:
+      "مجموعه‌ای از بهترین عینک‌های آفتابی و طبی با قیمت مناسب و کیفیت بالا.",
+    url: "https://yourdomain.com",
+    siteName: "عینک فروشی رهام",
+    images: [
+      {
+        url: "./images/photo_2026-02-14_01-04-33.jpg",
+        width: 1200,
+        height: 630,
+        alt: "عینک فروشی رهام",
+      },
+    ],
+    type: "website",
+  },
+};
+
 export default function Home() {
   return (
     <>
